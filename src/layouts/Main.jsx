cb704267acf51222aabc52fed1832fe07ddb0d92
ridhodaffasyah <ridhodaffasyah01@gmail.com>
2021-07-31 13:16:12 +0700

    add footer and css footer

--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import Navbar from './Navbar'
+import Footer from './Footer'
 
 const Main = () => {
     return (
         <>
             {/*isi disini*/}
             <Navbar></Navbar>
+
+            <Footer></Footer>
         </>
     )
 }
