5ece2948303ac7be93cb259537ed5145acfd008c
ridhodaffasyah <ridhodaffasyah01@gmail.com>
2021-07-31 22:28:54 +0700

    add homepage

--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -3,13 +3,14 @@ import Navbar from './Navbar'
 import Footer from './Footer'
 import Login from '../pages/Login'
 import Appbar from './Appbar'
+import Home from '../pages/home'
 
 const Main = () => {
     return (
         <>
             {/*isi disini*/}
             <Appbar />
-            <Login />
+            <Home></Home>
             <Footer></Footer>
         </>
     )
